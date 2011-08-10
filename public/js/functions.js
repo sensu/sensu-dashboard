c94ae9f86a5a89ecd3e8751ccc6e930cc9d69082
Justin Kolberg <amd.prophet@gmail.com>
2011-08-10 12:28:44 -0700

    clipboard support for event details

--- public/js/functions.js
+++ public/js/functions.js
@@ -38,9 +38,32 @@ function fetchAlerts() {
         $('#eventDetailsTemplate').tmpl(alerts_crit).appendTo('#event_detail_modals');
         $('#eventDetailsTemplate').tmpl(alerts_warn).appendTo('#event_detail_modals');
         $('tr[rel*=leanModal]').leanModal({ top : 50 });
+
+        // Add events to the generated templates
+        for (var a in alerts_crit) {
+          var identifier = alerts_crit[a]['client']+'_'+alerts_crit[a]['check'];
+          addClipboardToDetails(identifier);
+        }
+        for (var a in alerts_warn) {
+          var identifier = alerts_warn[a]['client']+'_'+alerts_warn[a]['check'];
+          addClipboardToDetails(identifier);
+        }
       });
     }
   });
+}
+
+function addClipboardToDetails(identifier) {
+
+  $('div#'+identifier+'_modal > div.alert_detail_group > div.copy').click(function() {
+    var currentVal = $(this).parent().find('div.alert_detail').children().last().text();
+
+    $(this).zclip({
+      path:'swf/ZeroClipboard.swf',
+      copy:currentVal
+    });
+  });
+
 }
 
 /* trigger when page is ready */
