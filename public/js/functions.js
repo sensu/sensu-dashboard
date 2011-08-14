fce11435b10da6d2c796b7d6550ca12d09a9a9dd
Justin Kolberg <amd.prophet@gmail.com>
2011-08-14 10:54:23 -0400

    simplified and reduce loc for handling events

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,14 +1,12 @@
 // remap jQuery to $
 (function($){})(window.jQuery);
 
-var alerts_crit, alerts_warn, alerts_unknown;
+var events;
 
 function fetchAlerts() {
   $.getJSON('/events', function(data) {
 
-    alerts_crit = new Array();
-    alerts_unknown = new Array();
-    alerts_warn = new Array();
+    m_events = new Array();
 
     $('table#alerts > tbody').empty();
     $('#event_detail_modals').empty();
@@ -16,67 +14,38 @@ function fetchAlerts() {
     for (var nodekey in data) {
       var node = data[nodekey];
 
-      $.getJSON('/client/'+nodekey, function(client_data) {
-        for (var a in node) {
+      for (var a in node) {
 
-          var dataObject = {
-            client: nodekey,
-            check: a,
-            status: node[a]['status'],
-            output: node[a]['output'],
-            environment: 'rackspace',
-            hostname: 'ip_10-20-10-20',
-            address: client_data['address'],
-            roles: client_data['subscriptions']
-          };
+        var dataObject = {
+          client: nodekey,
+          check: a,
+          status: node[a]['status'],
+          output: node[a]['output'],
+        };
 
-          if (node[a]['status'] == 1) {
-            alerts_warn.push(dataObject);
-          }
-          else if (node[a]['status'] == 2) {
-            alerts_crit.push(dataObject);
-          } else {
-            alerts_unknown.push(dataObject);
-          }
+        if (m_events[node[a]['status']] == null) {
+          m_events[node[a]['status']] = new Array();
         }
 
-        $('#critEventTemplate').tmpl(alerts_crit).appendTo('table#alerts > tbody');
-        $('#unknownEventTemplate').tmpl(alerts_unknown).appendTo('table#alerts > tbody');
-        $('#warnEventTemplate').tmpl(alerts_warn).appendTo('table#alerts > tbody');
-        $('#eventDetailsTemplate').tmpl(alerts_crit).appendTo('#event_detail_modals');
-        $('#eventDetailsTemplate').tmpl(alerts_unknown).appendTo('#event_detail_modals');
-        $('#eventDetailsTemplate').tmpl(alerts_warn).appendTo('#event_detail_modals');
-        $('tr[rel*=leanModal]').leanModal({ top : 50 });
-
-        // Add events to the generated templates
-        for (var a in alerts_crit) {
-          var identifier = alerts_crit[a]['client']+'_'+alerts_crit[a]['check'];
-          addClipboardToDetails(identifier);
-        }
-        for (var a in alerts_unknown) {
-          var identifier = alerts_unknown[a]['client']+'_'+alerts_unknown[a]['check'];
-          addClipboardToDetails(identifier);
-        }
-        for (var a in alerts_warn) {
-          var identifier = alerts_warn[a]['client']+'_'+alerts_warn[a]['check'];
-          addClipboardToDetails(identifier);
-        }
-      });
+        m_events[node[a]['status']].push(dataObject);
+      }
     }
+
+    for (status in m_events) {
+      for (a in m_events[status]) {
+        var m_event = m_events[status][a];
+
+        $('#eventTemplate').tmpl(m_event).prependTo('table#alerts > tbody');
+
+        $('tr#' + m_event['client'] + '_' + m_event['check']).click(function() {
+          // TODO: replace or clear & generate the contents of the modal dialog
+        });
+      }
+    }
+
+    $('tr[rel*=leanModal]').leanModal({ top : 50 });
+
   });
-}
-
-function addClipboardToDetails(identifier) {
-
-  $('div#'+identifier+'_modal > div.alert_detail_group > div.copy').click(function() {
-    var currentVal = $(this).parent().find('div.alert_detail').children().last().text();
-
-    $(this).zclip({
-      path:'swf/ZeroClipboard.swf',
-      copy:currentVal
-    });
-  });
-
 }
 
 /* trigger when page is ready */
@@ -88,6 +57,16 @@ $(document).ready(function () {
   ws.onmessage = function(evt) {
     fetchAlerts();
   }
+
+  // TODO: fix clipboard support
+  /*$('div#event_details_modal > div.alert_detail_group > div.copy').click(function() {
+    var currentVal = $(this).parent().find('div.alert_detail').children().last().text();
+
+    $(this).zclip({
+      path:'swf/ZeroClipboard.swf',
+      copy:currentVal
+    });
+  });*/
 
 });
 
